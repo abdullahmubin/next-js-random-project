576b638b9914578eb62d7bdc1bd66965da01bae6
Mubin Codemen <codemen.almubin@gmail.com>
2023-11-19 14:02:53 +0600

    trpc added.

---
+++ src/app/auth-callback/page.tsx
@@ -0,0 +1,13 @@
+import { useRouter, useSearchParams } from "next/navigation";
+
+const Page = async () => {
+    const router = useRouter();
+
+    const searchParams = useSearchParams();
+    const origin = searchParams.get('origin');
+
+    const apiResponse = await fetch('/api/whatever')
+
+}
+
+export default Page;
---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,24 @@
+// 'use client'
+
+// import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/dist/types/server";
+
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { redirect } from "next/navigation";
+
+const Page = async () => {
+    const { getUser } = getKindeServerSession()
+    const user = await getUser()
+    console.log('user')
+
+    if (!user || !user.id) redirect("/auth-callback?origin=dashboard")
+    // https://kinde.com/docs/developer-tools/nextjs-sdk/
+    // console.log(user.email)
+    // console.log(getUser().isRevalidate);
+    console.log(user?.email);
+
+
+
+    return (<div>hi {user?.email} bye</div>)
+}
+
+export default Page;
